369504624c643177ed2d5536a821eaaa05c8b4ff
ciskoh <matteo.jriva@gmail.com>
2021-03-22 11:00:56 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='our entry for the cool farm alliance challenge',
+    author='matteo jucker riva',
+    license='',
+)
