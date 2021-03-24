ae1c013b802fd1df26673e3729b1533baf54b971
markusgi <markusgig@gmail.com>
2021-03-24 16:57:09 +0100

    django map model first

--- maps/admin.py
+++ maps/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Map
+
 # Register your models here.
+
+admin.site.register(Map)
--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Map(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+    data = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f'{self.pk}: {self.name}'
