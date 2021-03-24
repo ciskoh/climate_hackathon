6810c0c314643d9e601f78b6fb1caebfefb83749
ciskoh <matteo.jriva@gmail.com>
2021-03-24 12:46:29 +0100

    added function refine_glc to translate glc for certain pixels

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,94 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
+
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+import zipfile
+import gdal
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+def create_geoTiff(dst_filepath, fin_array, data0):
+    x_pixels = fin_array.shape[1]  # number of pixels in x
+    y_pixels = fin_array.shape[0]  # number of pixels in y
+    driver = gdal.GetDriverByName('GTiff')
+    dataset = driver.Create(dst_filepath, x_pixels, y_pixels, 1, gdal.GDT_Float32)
+    dataset.GetRasterBand(1).WriteArray(fin_array)
+
+    # follow code is adding GeoTranform and Projection
+    geotrans = data0.GetGeoTransform()  # get GeoTranform from existed 'data0'
+    proj = data0.GetProjection()  # you can get from a exsited tif or import
+    dataset.SetGeoTransform(geotrans)
+    dataset.SetProjection(proj)
+    dataset.FlushCache()
+    dataset = None
+    if os.path.exists(dst_filepath):
+        print(f" saved geotiff at \n {dst_filepath}")
+    return None
+
+
+def refine_glc(input_file_name=None, refine=60):
+
+    print("preparing global land cover map")
+    # find latest file
+    if input_file_name == None:
+        raw_data_path = Path("..", "..", "data", "raw")
+        file_list = [f for f in os.listdir( raw_data_path ) if "global_land_cover" in f and f.endswith(".zip")]
+        input_file_name = sorted(file_list, reverse=True)[0]
+
+    # unzip file in raw folder
+    input_filepath = raw_data_path / input_file_name
+    unzip_folder = raw_data_path / input_file_name.split(".")[-2]
+    if not os.path.isdir(unzip_folder):
+        os.mkdir(unzip_folder)
+        print(input_filepath, unzip_folder)
+
+    with zipfile.ZipFile(input_filepath, 'r') as zip_ref:
+        zip_ref.extractall(unzip_folder)
+
+    # mask relevant bands with predict_proba band
+    if refine > 0:
+        print("refining classification information for ")
+        for r,_,fi in os.walk(unzip_folder):
+            for f in fi:
+                if "proba" in f:
+                   pred_proba_path = Path(r) / f
+                   print("found proba")
+                elif "classification.tif" in f:
+                    classi_path = Path(r) / f
+                    print("found discrete_classification")
+        # import arrays
+        proba_array = gdal.Open(str(pred_proba_path)).ReadAsArray()
+        classi_geofile = gdal.Open(str(classi_path))
+        classi_array = classi_geofile.ReadAsArray()
+
+        classi_array[proba_array < refine] = -99
+        print(np.sum(proba_array<refine) )
+        refined_array = np.where(proba_array > refine, classi_array, -99)
+        fig, ax = plt.subplots()
+        ax.imshow(refined_array)
+        print(np.unique(refined_array))
+
+        # save refined array as geotiff
+        dst_filepath= Path ("..", "..", "data", "processed" ) / str(input_file_name.split(".")[-2]+".tiff")
+        create_geoTiff(str(dst_filepath), refined_array, classi_geofile)
+
+
+
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
-    main()
+    refine_glc()
+
--- src/data/retrieve_data.py
+++ src/data/retrieve_data.py
@@ -20,8 +20,8 @@ def get_gee_data(aoi, date_range=["2020-05-01", "2020-07-01"], mode="sentinel_ra
     """download images from google earth engine as zip file
     aoi : area of interest as json file
     date_range : list of [start date, end_date] in 'YYYY-MM-DD' format
-    mode : sentinel_raw is the only implemented for now
-    band_names : list of band to keep from the original image defaults to ["B2", "B3", "B4", "B8"]
+    mode : 'sentinel_raw' for satellite images, 'global_land_cover' for copernicus glc maps
+    band_names : only for mode == sentinel_raw. List of band to keep from the original image defaults to ["B2", "B3", "B4", "B8"]
     Returns None. saves zip file with image in data/raw folder """
     # Initialize the Earth Engine module.
     try:
@@ -35,13 +35,12 @@ def get_gee_data(aoi, date_range=["2020-05-01", "2020-07-01"], mode="sentinel_ra
     # Area of interest as gee object
     coords_list = coords['features'][0]["geometry"]["coordinates"][0][0]
     aoi_obj = ee.Geometry.Polygon(coords_list)
-    print(f"Downloading {mode} image for coordinates {coords_list}")
 
+    print(f"Downloading {mode} image for coordinates {coords_list}")
     # date_range as gee object
     start_date = ee.Date(date_range[0])
     end_date = ee.Date(date_range[1])
-    if mode=="sentinel_raw":
-
+    if mode == "sentinel_raw":
         # get sentinel collection
         sent2 = ee.ImageCollection(ee.ImageCollection("COPERNICUS/S2_SR"))
         sent_coll = sent2.filterBounds(aoi_obj).filterDate(start_date,end_date)
@@ -49,37 +48,37 @@ def get_gee_data(aoi, date_range=["2020-05-01", "2020-07-01"], mode="sentinel_ra
         # map function over collection
         cloud_free_coll = sent_coll.map(mask_clouds)
         # merge image using mean
-        m_img = cloud_free_coll.mean().select(band_names)
+        fin_img = cloud_free_coll.mean().select(band_names)
+
+    # download global land cover
+    if mode == "global_land_cover":
+        glc = ee.ImageCollection("COPERNICUS/Landcover/100m/Proba-V-C3/Global")
+        fin_img = ee.Image(glc.toList(10).reverse().get(0)).clip(aoi_obj)
+
         # download image
-
-        link = m_img.getDownloadURL({
-            'scale': 10,
-            'crs': 'EPSG:4326',
-            'fileFormat': 'GeoTIFF',
-            'region': aoi_obj})
-        response = requests.get(link)
-        if not response.status_code == 200:
-            print(f"problem retrieving file from the link:\n {link})!")
-            return None
-        # set output filename
-        timestamp = re.sub("[^0-9]", "", dt.now().isoformat())
-        print("timestamp:", timestamp)
-        out_file = Path("..", "..", "data", "raw", "sentinel_raw_"+timestamp+".zip")
-        print("outfile:", out_file)
-        with open(out_file, "wb") as f:
-            f.write(response.content)
-        del response
-        if os.path.exists(out_file):
-            print(f"COMPLETED! image downloaded as zip file in {out_file}")
-
-
+    link = fin_img.getDownloadURL({
+        'scale': 10,
+        'crs': 'EPSG:4326',
+        'fileFormat': 'GeoTIFF',
+        'region': aoi_obj})
+    response = requests.get(link)
+    if not response.status_code == 200:
+        print(f"problem retrieving file from the link:\n {link})!")
+        return None
+    # set output filename
+    timestamp = re.sub("[^0-9]", "", dt.now().isoformat())
+    out_file = Path("..", "..", "data", "raw", mode + timestamp + ".zip")
+    with open(out_file, "wb") as f:
+        f.write(response.content)
+    del response
+    if os.path.exists(out_file):
+        print(f"COMPLETED! image downloaded as zip file in \n {out_file}")
 
 if __name__ == '__main__':
-    import json
-
     data_path = Path("..", "..", "data", "raw", "test_aoi_valencia.geojson")
-
     with open(data_path) as f:
         coords = json.load(f)
-
+    print("testing sentinel_raw....")
     get_gee_data(aoi=coords)
+    print("testing global land cover...")
+    get_gee_data(aoi=coords, mode="global_land_cover")
