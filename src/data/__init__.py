5a6646fee4f5ea05f6ff678c2b0feff3ffc902af
ciskoh <matteo.jriva@gmail.com>
2021-03-24 23:04:14 +0100

    completed functions to download and preprocess dataset added function create_and_preprocess_dataset

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,11 @@
 """ modules to download data and preprocess it"""
 
 # download data
-from .retrieve_data import get_gee_data
+from .retrieve_data import download_dataset
 # preprocess data
 from .make_dataset import make_dataset
+
+def create_and_preprocess_dataset(aoi_path):
+    dataset_name = download_dataset(aoi_path)
+    make_dataset(dataset_name)
+    print(f"dataset {dataset_name} downloaded and preprocessed")
